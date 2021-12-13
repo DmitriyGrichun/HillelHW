b3fbdab9f64c743fbb160879b453d68a6d449842
DmitriyGrichun <grichun@gmail.com>
2021-12-13 20:02:21 +0200

    Home Work 5.2. Home Work 5.3.

--- Home_Work_5/Home_Work_5.2.py
+++ Home_Work_5/Home_Work_5.2.py
@@ -1,7 +1,7 @@
 
-n = list(range(10))
-print(n)
-m = []
-for item in n:
-        m = [item]
-        print("coppy", m)
+numbers_list_1 = list(range(10))
+print(numbers_list_1)
+copy_numbers_list_1 = []
+for i in numbers_list_1:
+      copy_numbers_list_1.append(i)
+print(copy_numbers_list_1)

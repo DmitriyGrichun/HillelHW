f098cf025466c255c825e28d5d30a8205ed66dd4
DmitriyGrichun <grichun@gmail.com>
2021-12-13 20:45:10 +0200

    Home Work 5.4. Home Work 5.5.

--- Home_Work_5/Home_Work_5.2.py
+++ Home_Work_5/Home_Work_5.2.py
@@ -5,3 +5,4 @@ copy_numbers_list_1 = []
 for i in numbers_list_1:
       copy_numbers_list_1.append(i)
 print(copy_numbers_list_1)
+
---
+++ Home_Work_5/Home_Work_5.4.py
@@ -0,0 +1,6 @@
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dict4 = (list(dic1.items()) + list(dic2.items()) + list(dic3.items()))
+print(dict4)

1018ef75c4a55755d1fe1156014630be1508d849
DmitriyGrichun <grichun@gmail.com>
2021-12-21 17:43:43 +0200

    Home Work 6.8. Home Work 6.9.

---
+++ Home_Work_6/Home_Work_6.8.py
@@ -0,0 +1,5 @@
+
+numbers_list_1 = set([1, 3, 5, 7, 9, 2, 2, 4, 4])
+numbers_list_2 = set([2, 4, 6, 8, 10, 10, 3, 345])
+numbers_list_3 = set.union(numbers_list_1.union(numbers_list_2))
+print(numbers_list_3)
